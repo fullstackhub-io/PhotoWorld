c0b44959c59762ef9eb9162de8a1d935b312aba4
fullstackhub-io <fullstackhub@yahoo.com>
2019-05-12 18:29:01 -0500

    Added SRS and Database script.
    Added SRS and Database script.

---
+++ PhotoWorld/Scripts/Common.js
@@ -0,0 +1,37 @@
+﻿$(document).ready(function () {
+
+    $('[data-toggle="tooltip"]').tooltip();
+
+    $(document).ajaxStart(function () {
+        $("#pluswrap").show();
+    });
+    $(document).ajaxStop(function () {
+        $("#pluswrap").hide();
+    });
+});
+
+(function ($) {
+
+    //re-set all client validation given a jQuery selected form or child
+    $.fn.resetValidation = function () {
+
+        var $form = this.closest('form');
+
+        //reset jQuery Validate's internals
+        $form.validate().resetForm();
+
+        //reset unobtrusive validation summary, if it exists
+        $form.find("[data-valmsg-summary=true]")
+            .removeClass("validation-summary-errors")
+            .addClass("validation-summary-valid")
+            .find("ul").empty();
+
+        //reset unobtrusive field level, if it exists
+        $form.find("[data-valmsg-replace]")
+            .removeClass("field-validation-error")
+            .addClass("field-validation-valid")
+            .empty();
+
+        return $form;
+    };
+})(jQuery);
